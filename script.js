3ba4e6ec35c5741f3bd23ab45e8cdf750e89ce10
zob-cloud <joehanmathewbino3@gmail.com>
2026-04-08 01:15:41 +0530

    upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  let menu = document.getElementById("menu");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
